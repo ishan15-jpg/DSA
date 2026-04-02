294208446ee3c2c0ab976c0b0f5c81421552e1b4
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-04-02 08:26:34 +0530

    Time: 0 ms (100%), Space: 13.5 MB (13.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,20 +3,17 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         size_t n1 = nums1.size(), n2 = nums2.size();
 
-        for(int i=0; i<n1; i++){
-            int max_ = -1;
-            for(int j=0; j<n2; j++){
-                if(nums1[i] == nums2[j]){
-                    for(int k=j+1; k<n2; k++){
-                        if(nums2[k] > nums2[j]){
-                            max_ = nums2[k];
-                            break;
-                        }
-                    }
-                }
-            }
-            nums1[i] = max_;
+        vector<int> max_(10001, -1);
+        stack<int> s;
+
+        for(int i=n2-1; i>=0; i--){
+            while(!s.empty() && nums2[i] >= s.top()) s.pop();
+
+            if(!s.empty()) max_[nums2[i]] = s.top();
+            s.push(nums2[i]);
         }
+
+        for(int i=0; i<n1; i++) nums1[i] = max_[nums1[i]];
 
         return nums1;
     }
