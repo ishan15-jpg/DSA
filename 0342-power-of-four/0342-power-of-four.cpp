6f7b7d1f6742e3c836f7e49b2458b75397fc1956
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-04-08 10:31:09 +0530

    Time: 0 ms (100%), Space: 8 MB (11.87%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -3,13 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if(n <= 0) return false;
         if(n == 1) return true;
-
-        while(n > 0){
-            if(n == 1) break;
-            if(n%4 > 0) return false;
-            n /= 4;
-        } 
-
-        return true;
+        if(n%4 >= 1) return false;
+        return isPowerOfFour(n/4);
     }
 };
