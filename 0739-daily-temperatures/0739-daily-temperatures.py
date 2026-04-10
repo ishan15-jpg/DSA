8f3acc794b1cf24df1198e17fc8e8db41ee508e8
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-04-10 10:03:42 +0530

    Time: 83 ms (91.71%), Space: 28.9 MB (38.02%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,14 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        if n == 1 : return [0]
+        st,answer = list(), [0]*len(temperatures)
 
-        st,answer = list(), [0]*n
+        for i,t in enumerate(temperatures):
+            while st and t > temperatures[st[-1]]:
+                prev_day = st.pop()
+                answer[prev_day] = i - prev_day
 
-        for i in range(0,n):
-            while len(st) > 0 and temperatures[i] > temperatures[st[-1]]:
-                answer[st[-1]] = i - st[-1]
-                st.pop(-1)
             st.append(i)
 
         return answer
