14fbfe08a03125757811a34a99c44ce797446cad
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-06-25 09:55:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (48.49%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -3,24 +3,22 @@ public:
     int numDecodings(string s) {
         size_t n = s.length();
 
-        vector<int> dp(n+1, 0);
-
-        dp[n] = 1;
+        int dp = 0, dp1 = 1, dp2 = 0;
 
         for(int i=n-1; i>=0; --i){
-            if(s[i] == '0') continue;
-
-            int one = dp[i+1];
+            if(s[i] == '0') dp = 0;
+            else dp = dp1;
             
-            int two = 0;
             if(i+2 <= n){
                 int num = stoi(s.substr(i,2));
-                two = (num >= 10 && num <= 26) ? dp[i+2] : 0;
+                dp += (num >= 10 && num <= 26) ? dp2 : 0;
             }
 
-            dp[i] = one + two;
+            dp2 = dp1;
+            dp1 = dp;
+            dp = 0;
         }        
 
-        return dp[0];
+        return dp1;
     }
 };
