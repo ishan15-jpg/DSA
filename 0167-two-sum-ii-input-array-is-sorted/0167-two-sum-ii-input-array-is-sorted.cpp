cf52c03287155a98dd9090ee12870a6252e6f26b
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-06 08:52:02 +0530

    Time: 0 ms (100%), Space: 19.3 MB (95.25%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,23 +3,12 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         size_t n = numbers.size();
 
-        function<int(int,int)> findRem = [&](int rem, int i) -> int {
-            int l = i, h = n-1;
+        int l = 0, r = n-1;
 
-            while(l <= h){
-                int mid = (l + h) >> 1;
-                if(numbers[mid] == rem) return mid;
-                else if(numbers[mid] < rem) l = mid + 1;
-                else h = mid - 1;
-            }
-
-            return -1;
-        };
-
-        for(int i=0; i<n; i++){
-            int rem = target - numbers[i];
-            int idx = findRem(rem,i+1);
-            if(idx != -1) return {i+1, idx+1};
+        while(l < r){
+            if(numbers[l] + numbers[r] == target) return {l+1,r+1};
+            else if(numbers[l] + numbers[r] < target) l++;
+            else r--;
         }
 
         return {-1,-1};
