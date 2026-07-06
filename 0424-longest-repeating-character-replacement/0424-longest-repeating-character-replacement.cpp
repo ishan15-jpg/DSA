def1feeda43d18031e87c8efca2bcfe867eaf5cf
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-06 10:12:38 +0530

    Time: 1 ms (75.86%), Space: 10.8 MB (93.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         size_t n = s.length();
-        unordered_set<int> st;
-        for(const char c : s) st.insert(c - 'A');
-        int max_ = 0;
-        for(const int c : st){
-            vector<int> freq(26,0);
-            int maxFreq = 0, l = 0;
-            for(int r=0; r<n; ++r){
-                ++freq[s[r] - 'A'];
-                maxFreq = max(maxFreq, freq[c]);
-                while(l<=r && r-l+1-maxFreq > k){
-                    --freq[s[l] - 'A'];
-                    ++l; 
-                }
-                max_ = max(max_, r-l+1);
+        int l = 0, maxFreq = 0, max_ = 0;
+        vector<int> freq(26,0);
+        for(int r=0; r<n; ++r){
+            maxFreq = max(maxFreq, ++freq[s[r] - 'A']);
+            while(l<=r && r-l+1-maxFreq > k){
+                --freq[s[l] - 'A'];
+                ++l;
             }
+            max_ = max(max_, r-l+1);
         }
         return max_;
     }
