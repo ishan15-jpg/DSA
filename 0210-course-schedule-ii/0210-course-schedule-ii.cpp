9a5f9b0a079fd0fae564e638d428c2e1e7b9fa0e
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-08 08:31:56 +0530

    Time: 10 ms (14.04%), Space: 18.9 MB (34.42%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adjList(numCourses);
-        for(const vector<int> prerequisite : prerequisites)
-        adjList[prerequisite[0]].push_back(prerequisite[1]);
+        vector<int> indegree(numCourses, 0);
+        for(const vector<int> prerequisite : prerequisites){
+            adjList[prerequisite[0]].push_back(prerequisite[1]);
+            ++indegree[prerequisite[1]];
+        }
 
-        vector<bool> visited(numCourses, false);
-        vector<bool> stackTrace(numCourses, false);
+        queue<int> q;
+        for(int c=0; c<numCourses; ++c) 
+        if(indegree[c] == 0) q.push(c);
+
         vector<int> answer;
 
-        function<bool(int)> dfs = [&](int course) -> bool {
-            visited[course] = stackTrace[course] = true;
-            for(const int rc : adjList[course]){
-                if(visited[rc] && stackTrace[rc]) return true;
-                if(!visited[rc] && dfs(rc)) return true;
+        while(!q.empty()){
+            int c = q.front(); q.pop();
+            answer.push_back(c);
+            for(const int rc : adjList[c]){
+                --indegree[rc];
+                if(indegree[rc] == 0) q.push(rc);
             }
-            stackTrace[course] = false;
-            answer.push_back(course);
-            return false;
-        };
+        }
 
-        for(int c=0; c<numCourses; ++c)
-        if(!visited[c] && dfs(c)) return {}; 
+        if(answer.size() < numCourses) return {};
+
+        reverse(answer.begin(), answer.end());
 
         return answer;
     }
