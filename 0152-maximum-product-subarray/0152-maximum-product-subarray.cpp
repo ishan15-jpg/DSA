46becd66e419b2c4aae09c0fba71eec72cd20ce4
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-09 09:49:01 +0530

    Time: 0 ms (100%), Space: 17.6 MB (86.78%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int max_ = nums[max_element(nums.begin(),nums.end()) - nums.begin()];
+       int currMax = 1, currMin = 1, max_ = nums[0];
 
-        vector<vector<int>> segments;
-        vector<int> temp;
-        for(const int num : nums){
-            if(num == 0){
-                if(!temp.empty()){
-                    segments.push_back(temp);
-                    temp.clear();
-                }
-            }else temp.push_back(num);
-        }
-        segments.push_back(temp);
+       for(const int num : nums){
+            int temp = currMax * num;
+            currMax = max(num, max(currMax*num, currMin*num));
+            currMin = min(num, min(temp, currMin*num));
+            max_ = max(max_,currMax);
+       }
 
-        for(const vector<int>& segment : segments){
-            int prd = 1, negs = 0, i = 0;
-            size_t n = segment.size();
-            for(const int num : segment) if(num < 0) ++negs;
-            int need = negs%2 == 0 ? negs : negs - 1;
-            negs = 0;
-            
-            for(int j=0;j<n;++j){
-                prd *= segment[j];
-                if(segment[j] < 0){
-                    ++negs;
-                    while(negs > need){
-                        prd /= segment[i];
-                        if(segment[i] < 0) --negs;
-                        ++i;
-                    }
-                }
-                if(i<=j) max_ = max(max_, prd);
-            }
-        }
-
-        return max_;
+       return max_;
     }
 };
