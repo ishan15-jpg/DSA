8373e081663bd103636f16e8516e77f5c4065859
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-11 08:29:41 +0530

    Time: 23 ms (54.24%), Space: 80.2 MB (85.55%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -3,24 +3,22 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         size_t n = heights.size();
 
-        stack<int> nst; stack<int> pst;
-        vector<int> nse(n,n); vector<int> pse(n,-1);
-
-        for(int i=0; i<n; ++i){
-            while(!pst.empty() && heights[i] <= heights[pst.top()]) pst.pop();
-            if(!pst.empty()) pse[i] = pst.top();
-            pst.push(i);
-
-            while(!nst.empty() && heights[n-1-i] <= heights[nst.top()]) nst.pop();
-            if(!nst.empty()) nse[n-1-i] = nst.top();
-            nst.push(n-1-i);
-        }
-
+        stack<pair<int,int>> st;
         int max_ = 0;
 
         for(int i=0; i<n; ++i){
-            int left = pse[i]+1, right = nse[i]-1;
-            max_ = max(max_, (right - left + 1) * heights[i]); 
+            int start = i;
+            while(!st.empty() && st.top().first >= heights[i]){
+                auto [height,index] = st.top(); st.pop();
+                max_ = max(max_, (i-index) * height);
+                start = index;
+            }
+            st.push({heights[i],start});
+        }
+
+        while(!st.empty()){
+            auto [height,index] = st.top(); st.pop();
+            max_ = max(max_, ((int)n - index) * height);
         }
 
         return max_;
