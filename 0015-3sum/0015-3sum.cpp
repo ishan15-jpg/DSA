bf45748647a4b4fc0e98dcf61904d76cb8a657c9
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-13 10:37:19 +0530

    Time: 39 ms (95.16%), Space: 29.2 MB (47.61%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,19 +7,19 @@ public:
 
         vector<vector<int>> answer;
 
-        unordered_map<int,int> mp;
-        for(const int num : nums) mp[num]++;
-
-        for(int i=0; i<n-1; ++i){
-            --mp[nums[i]];
+        for(int i=0; i<n; ++i){
+            if(nums[i] > 0) break;
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            for(int j=i+1; j<n; ++j){
-                --mp[nums[j]];
-                if(j-1 > i && nums[j] == nums[j-1]) continue;
-                int target = -(nums[i] + nums[j]);
-                if(mp.find(target) != mp.end() && mp[target] > 0) answer.push_back({nums[i],nums[j],target});
+            int l = i+1, r = n-1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == 0){
+                    answer.push_back({nums[i], nums[l], nums[r]});
+                    ++l; --r;
+                    while(l < r && nums[l] == nums[l-1]) ++l;
+                }else if(sum < 0) ++l;
+                else --r;
             }
-            for(int j=i+1; j<n; ++j) ++mp[nums[j]];
         }
 
         return answer;
