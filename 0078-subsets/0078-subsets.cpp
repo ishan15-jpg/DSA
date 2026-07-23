25cf3b48aa35ff2c7400fbbdbecc05b4c411d7b5
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-23 09:15:30 +0530

    Time: 3 ms (27.68%), Space: 9.8 MB (82.58%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,12 +3,14 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         size_t n = nums.size();
 
-        vector<vector<int>> answer = {{}};
+        if(n == 1) return {{},{nums[0]}};
 
-        for(int i=1; i<(1<<n); i++){
+        vector<vector<int>> answer{{}};
+
+        for(int i=1; i<(1 << n); ++i){
             vector<int> temp;
-            for(int j=0; j<n; j++){
-                if(i & (1 << j)) temp.push_back(nums[j]);
+            for(int j=0; j<n; ++j){
+                if((1 << j) & i) temp.push_back(nums[j]);
             }
             answer.push_back(temp);
         }
