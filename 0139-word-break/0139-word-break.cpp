4b7a459982e75a012d5aed03eb514a68dc7866aa
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-24 10:23:03 +0530

    Time: 15 ms (28.17%), Space: 17.7 MB (18.68%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -3,14 +3,15 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         size_t l = s.length();
 
+        unordered_set<string> st;
+        for(const string word : wordDict) st.insert(word);
+
         vector<bool> dp(l+1, false);
         dp[l] = 1;
 
         for(int i=l-1; i>=0; --i){
-            for(const string word : wordDict){
-                int l_ = word.length();
-                if(l-i < l_) continue;
-                if(s.substr(i,l_) == word && dp[i+l_]) dp[i] = true;
+            for(int j=1; j<l-i+1; ++j){
+                if(st.find(s.substr(i,j)) != st.end() && dp[i+j]) dp[i] = true;
             }
         }
 
