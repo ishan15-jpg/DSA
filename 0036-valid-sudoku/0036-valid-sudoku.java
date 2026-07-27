6263ed5587ba5e82c26dc6c7f751c0d27d6d7b74
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-27 09:36:06 +0530

    Time: 1 ms (99.93%), Space: 45.9 MB (98.88%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -2,32 +2,19 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         int rows = board.length, cols = board[0].length;
 
+        int[][] rows_ = new int[10][10], cols_ = new int[10][10], squares = new int[9][10];
+
         for(int r=0; r<rows; ++r){
-            int[] seen = new int[10];
             for(int c=0; c<cols; ++c){
                 if(board[r][c] == '.') continue;
-                if(seen[board[r][c] - '0'] == 1) return false;
-                ++seen[board[r][c] - '0'];
-            }
-        }
+                int square = (r / 3) * 3 + (c / 3); 
+                if(rows_[r][board[r][c] - '0'] == 1 
+                || cols_[c][board[r][c] - '0'] == 1
+                || squares[square][board[r][c] - '0'] == 1) return false;
 
-        for(int c=0; c<cols; ++c){
-            int[] seen = new int[10];
-            for(int r=0; r<rows; ++r){
-                if(board[r][c] == '.') continue;
-                if(seen[board[r][c] - '0'] == 1) return false;
-                ++seen[board[r][c] - '0'];
-            }
-        }
-
-        for(int s=0; s<9; ++s){
-            int[] seen = new int[10];
-            for(int r=0; r<3; ++r)
-            for(int c=0; c<3; ++c){
-                int row = (s / 3) * 3 + r, col = (s % 3) * 3 + c;
-                if(board[row][col] == '.') continue;
-                if(seen[board[row][col] - '0'] == 1) return false;
-                ++seen[board[row][col] - '0'];
+                ++rows_[r][board[r][c] - '0'];
+                ++cols_[c][board[r][c] - '0'];
+                ++squares[square][board[r][c] - '0'];
             }
         }
 
