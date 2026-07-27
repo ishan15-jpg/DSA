e2268d645ecdcef51b60394882dd40a2f70d2891
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-07-27 10:24:22 +0530

    Time: 1 ms (99.93%), Space: 45.7 MB (99.66%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int rows = board.length, cols = board[0].length;
+        int rows = board.length, cols = board.length;
 
-        int[][] rows_ = new int[10][10], cols_ = new int[10][10], squares = new int[9][10];
+        int[] rows_ = new int[9], cols_ = new int[9], squares = new int[9];
 
         for(int r=0; r<rows; ++r){
             for(int c=0; c<cols; ++c){
                 if(board[r][c] == '.') continue;
-                int square = (r / 3) * 3 + (c / 3); 
-                if(rows_[r][board[r][c] - '0'] == 1 
-                || cols_[c][board[r][c] - '0'] == 1
-                || squares[square][board[r][c] - '0'] == 1) return false;
+                int val = (board[r][c] - '0') - 1;
+                int square = (r / 3) * 3 + (c / 3);
+                if(((1 << val) & rows_[r]) > 0
+                || ((1 << val) & cols_[c]) > 0
+                || ((1 << val) & squares[square]) > 0
+                ) return false;  
 
-                ++rows_[r][board[r][c] - '0'];
-                ++cols_[c][board[r][c] - '0'];
-                ++squares[square][board[r][c] - '0'];
+                rows_[r] |= (1 << val);
+                cols_[c] |= (1 << val);
+                squares[square] |= (1 << val);
             }
         }
 
