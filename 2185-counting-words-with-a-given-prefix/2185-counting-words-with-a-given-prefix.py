b4b8cf40edd50620b4be91c23cfbb217328526ad
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-08-29 13:27:24 +0530

    Time: 49 ms (5.31%), Space: 20.5 MB (5.1%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
@@ -1,15 +1,31 @@
-class Solution:
-    def isPrefix(self, pref: str, word: str) -> bool:
-        if len(pref) > len(word): return False
-        i,j = 0,0
-        while i < len(pref) and j < len(word):
-            if pref[i] != word[j]: return False
-            i += 1
-            j += 1
-        return True
+class TrieNode: 
+    def __init__(self):
+        self.children = {}
+        self.count = 0
 
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, words: List[str]):
+        for word in words:
+            curr = self.root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+                curr.count += 1
+
+    def prefixCount(self, pref: str) -> int:
+        curr = self.root
+        for p in pref: 
+            if p not in curr.children: return 0
+            curr = curr.children[p]
+        return curr.count
+
+class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
         res = 0
-        for word in words:
-            if self.isPrefix(pref,word): res += 1
-        return res
+        root = Trie()
+        root.add(words)
+        return root.prefixCount(pref)
