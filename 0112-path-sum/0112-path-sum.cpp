7385d9a83283d68ae2f78bd3046b9fc15b2b6da2
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-09-01 12:59:29 +0530

    Time: 0 ms (100%), Space: 21.6 MB (35.39%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -14,7 +14,6 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
         if(!root->left && !root->right) return targetSum - root->val == 0;
-
         return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
     }
 };
