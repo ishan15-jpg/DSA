43aaea6155a00b1a566d637da3ce6f77db624d38
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-09-03 12:39:44 +0530

    Time: 3 ms (70.06%), Space: 20.6 MB (26.9%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,12 +1,28 @@
 class Solution:
+    def rec(self, row: int, col: int, triangle: List[List[int]]) -> int:
+        if row == len(triangle): return 0
+        currCol = self.rec(row+1,col,triangle) 
+        nextCol = self.rec(row+1,col+1,triangle) 
+        return triangle[row][col] + min(currCol,nextCol)
+
+    def memo(self, row: int, col: int, dp: List[List[int]], triangle: List[List[int]]) -> int:
+        if row == len(triangle): return 0
+        if dp[row][col] is not None: return dp[row][col]
+        currCol = self.memo(row+1,col,dp,triangle) 
+        nextCol = self.memo(row+1,col+1,dp,triangle)
+        dp[row][col] = triangle[row][col] + min(currCol,nextCol)
+        return dp[row][col]
+
+    def tab(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[0] * (i+1) for i in range(n+1)]
+        for r in range(n-1,-1,-1):
+            for c in range(len(triangle[r])-1,-1,-1):
+                currCol,nextCol = dp[r+1][c],dp[r+1][c+1]
+                dp[r][c] = triangle[r][c] + min(currCol,nextCol)
+        return dp[0][0]
+
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        dp = [[None] * (i+1) for i in range(n)]
-        def memo(row: int, col: int) -> int:
-            if row == n: return 0
-            if dp[row][col] is not None: return dp[row][col]
-            currCol = memo(row+1,col) 
-            nextCol = memo(row+1,col+1)
-            dp[row][col] = triangle[row][col] + min(currCol,nextCol)
-            return dp[row][col]
-        return memo(0,0)
+        # dp = [[None] * (i+1) for i in range(n)]
+        return self.tab(triangle)
