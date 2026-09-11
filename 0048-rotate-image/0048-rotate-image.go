dcc0f1c52864876bcc7a2e547e6e1d5693ca2470
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-09-11 12:03:29 +0530

    Time: 0 ms (100%), Space: 4.1 MB (54.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,26 +1,14 @@
-func swap(matrix [][]int, i int, j int) {
-    var temp int = matrix[i][j]
-    matrix[i][j] = matrix[j][i]
-    matrix[j][i] = temp
-}
-
-func reverse(arr []int){
-    var n int = len(arr)
-    for i:=0; i<n/2; i++ {
-        var temp int = arr[i]
-        arr[i] = arr[n-i-1]
-        arr[n-i-1] = temp
-    }
-}
-
 func rotate(matrix [][]int)  {
-    var n int = len(matrix)
-    for i:=0; i<n-1; i++ {
-        for j:=i+1; j<n; j++ {
-            swap(matrix,i,j)
+   l,r := 0,len(matrix)-1
+   for l < r {
+        for i:=0; i<r-l; i++ {
+            top,bottom := l,r
+            topLeft := matrix[top][l+i]
+            matrix[top][l+i] = matrix[bottom-i][l]
+            matrix[bottom-i][l] = matrix[bottom][r-i]
+            matrix[bottom][r-i] = matrix[top+i][r]
+            matrix[top+i][r] = topLeft
         }
-    }
-    for i:=0; i<n; i++ {
-        reverse(matrix[i])
-    }
+        l++; r--
+   }
 }
