1b80c59f4e557dcce903e5622e38be2e7a58f19c
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-09-11 12:27:10 +0530

    Time: 0 ms (100%), Space: 10.2 MB (36.43%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        size_t n = matrix.size();
-        for(int i=0; i<n-1; ++i){
-            for(int j=i+1; j<n; ++j)
-                swap(matrix[i][j],matrix[j][i]);
+        int l = 0, r = matrix.size()-1;
+        while(l < r){
+            int top = l, bottom = r;
+            for(int i=0; i<r-l; ++i){
+                int topLeft = matrix[top][l+i];
+                matrix[top][l+i] = matrix[bottom-i][l];
+                matrix[bottom-i][l] = matrix[bottom][r-i];
+                matrix[bottom][r-i] = matrix[top+i][r];
+                matrix[top+i][r] = topLeft;
+            }
+            ++l; --r;
         }
-        for(int i=0; i<n; ++i)
-            reverse(matrix[i].begin(),matrix[i].end());
     }
 };
