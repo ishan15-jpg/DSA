f1851fcdbd348aba8c6f6fa1829f108899bf0073
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-09-11 16:16:27 +0530

    Time: 0 ms (100%), Space: 8 MB (91.04%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
@@ -0,0 +1,30 @@
+func setZeroes(matrix [][]int)  {
+    rows,cols := len(matrix),len(matrix[0])
+    rowZero := false
+    for r:=0; r<rows; r++ {
+        for c:=0; c<cols; c++ {
+            if matrix[r][c] == 0 {
+                matrix[0][c] = 0
+                if r == 0 { rowZero = true }
+                if r > 0 { matrix[r][0] = 0 }
+            }
+        }
+    }
+    for r:=1; r<rows; r++ {
+        for c:=1; c<cols; c++ {
+            if matrix[r][0] == 0 || matrix[0][c] == 0 {
+                matrix[r][c] = 0
+            }
+        }
+    }
+    if matrix[0][0] == 0 {
+        for r:=0; r<rows; r++ {
+            matrix[r][0] = 0
+        }
+    }
+    if rowZero {
+        for c:=0; c<cols; c++ {
+            matrix[0][c] = 0
+        }
+    }
+}
