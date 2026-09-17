1c9cbc1f48fff2e1baca3156388a1af88f9ec7fc
ishan15-jpg <dewanganishaan150324@gmail.com>
2026-09-17 12:31:42 +0530

    Time: 0 ms (100%), Space: 4.3 MB (99.76%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -0,0 +1,23 @@
+func search(nums []int, target int) int {
+    l,r := 0,len(nums)-1
+    for l < r {
+        mid := (l+r) >> 1
+        if nums[mid] > nums[r] {
+            l = mid + 1
+        } else { r = mid }
+    }
+    binarySearch := func(l int, r int) int {
+        for l <= r {
+            mid := (l+r) >> 1
+            if nums[mid] == target { 
+                return mid 
+            } else if nums[mid] < target {
+                l = mid+1
+            } else { r = mid-1}
+        }
+        return -1
+    }
+    result := binarySearch(0,l-1)
+    if result != -1 { return result }
+    return binarySearch(l,len(nums)-1)
+}
